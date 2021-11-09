76b6b1e34fdce1de5e9dd221a4cbb1c2c7dd3b06
Chris Adkins <adkinscda@gmail.com>
2021-11-08 23:59:32 -0800

    Added week11

---
+++ Week9/Document/Document.cs
@@ -0,0 +1,149 @@
+﻿/// Week 9 Lab 1
+/// File Name: Document.cs
+/// @author: Chris Adkins
+/// Date:11/8/21
+///
+/// Problem Statement: This program creates a base class as well as two sub classes that both inherit variables and overload methods from the base class.
+/// 
+/// Overall Plan
+/// 1. Create base class called Document with a variable to store a string.
+/// 2. Create two sub classes, Email which includes additional instance variables and File which includes a single additional instance variable.
+/// 3. Create a driver class to demonstrate the differences between the three classes.
+///
+/// Classes Needed and Purpose: 
+/// Main Class: Document, base class for Email and File.
+/// Sub Classes: File and Email, Subclasses of Document with additional instance variables and modified methods.
+/// Driver Class: DocumentDriver, Demonstrates the functionality of the three classes. 
+
+using System;
+
+namespace Document {
+    class DocumentDriver {
+        static void Main(string[] args) {
+
+            Email email1 = new Email();
+            Email email2 = new Email("Have you lost your mind? The CIA want me to launch a retaliatory strike, a blockade is the only way this ends peacefully.", 
+                                     "John F. Kennedy", "Fidel Castro", "What are you doing?");
+            File file1 = new File();
+            File file2 = new File("2+2=4", "C:/Users/billy/Documents/Mathquiz.txt");
+
+            static bool ContainsKeyword(Document docObject, string keyword) { // Provided parser method
+                if (docObject.ToString().IndexOf(keyword, 0) >= 0) {
+                    return true;
+                }
+                return false;
+            }
+
+             // Passing each object through a parser that checks for the word, "hello"
+            // email1 and file1 should be true because they use the default text which includes "hello"
+            Console.WriteLine(ContainsKeyword(email1, "Hello"));
+            Console.WriteLine(ContainsKeyword(email2, "Hello"));
+            Console.WriteLine(ContainsKeyword(file1, "Hello"));
+            Console.WriteLine(ContainsKeyword(file2, "Hello"));
+        }
+    }
+
+    class Document {
+        // Instance Variables
+        protected string text;
+
+        // Constructors
+        public Document() { // Default No Arg
+            text = "Hello. Goodbye!";
+        }
+
+        public Document(string aText) { // Constructor with argument
+            text = aText;
+        }
+
+        // Getters and Setters
+        private void setText (string aText) {
+            text = aText;
+        }
+
+        // Other Methods
+        public override string ToString() {
+            return text;
+        }
+    }
+
+    class Email : Document {
+        // Instance Variables
+        private string sender, recipient, title;
+
+        // Constructors
+        public Email() { // Default No Arg
+            sender = "Bradley Nowell";
+            recipient = "Eric Wilson";
+            title = "New Lyrics";
+            text = "Hello!";
+        }
+        
+        public Email(string aText, string aSender, string aRecipient, string aTitle) { // Constructor with argument
+            text = aText;
+            sender = aSender;
+            recipient = aRecipient;
+            title = aTitle;
+        }
+
+        // Getters and Setters
+        public string getSender() {
+            return sender;
+        }
+
+        private void setSender(string aSender) {
+            sender = aSender;
+        }
+
+        public string getRecipient() {
+            return recipient;
+        }
+
+        private void setRecipient(string aRecipient) {
+            recipient = aRecipient;
+        }
+
+        public string getTitle() {
+            return title;
+        }
+
+        private void setTitle(string aTitle) {
+            title = aTitle;
+        }
+
+        // Other Methods
+        public override string ToString() {
+            return "To: " + getRecipient() + "\nFrom: " + getSender() + "\nSubject: " + getTitle() + "\n Message: " + base.ToString();
+        }
+    }
+
+    class File : Document {
+        // Instance Variables
+        private string path;
+
+        // Constructors
+        public File() { // Default No Arg
+            path = "C:/Users/rickastleyfan420/Documents/Homework/Grapefruit.mp4";
+        }
+
+        public File(string aText, string aPath) { // Constructor with arguments
+            text = aText;
+            path = aPath;
+        }
+
+        // Getters and Setters
+        public string getPath() {
+            return path;
+        }
+
+        private void setPath(string aPath) {
+            path = aPath;
+        }
+
+        // Other Methods
+        public override string ToString() {
+            return "Path: " + getPath() + "\nText: " + base.ToString();
+        }
+    }
+
+}   
---
+++ Week9/Vehicles/vehicle.cs
@@ -0,0 +1,195 @@
+﻿/// Week 11 Homework 1
+/// File Name: vehicle,cs
+/// @author: Chris Adkins
+/// Date: 11/8/21
+///
+/// Problem Statement: This program creates a class called Vehicle and a sub class called Truck which has customer implementations of some base class methods.
+/// 
+/// Overall Plan
+/// 1. Create Vehicle class with instance variables, constructors, getters setters, and ToString methods.
+/// 2. Create sub class with additional instance variables and a new ToString() and Equals() method,
+/// 3. Create a driver class to demonstrate the differences between the classes.
+///
+/// Classes Needed and Purpose: 
+/// Main Class: Vehicle, base class for Truck
+/// Sub Class: Truck, Sub Class of Vehicle
+/// Driver Class: Vehicle Driver (lol), Class to demonstrate methods and constructors of each class
+
+using System;
+
+namespace Vehicles {
+
+
+
+    class VehicleDriver {
+        static void Main(string[] args) {
+            Vehicle car1 = new Vehicle(Manufacturer.Ford, 8, new Person("Bob Dylan"));
+            Vehicle car2 = new Vehicle();
+            Truck truck2 = new Truck();
+            Truck truck1 = new Truck(Manufacturer.Toyota, 6, new Person("Joe Dirt"), 500, 600);
+
+            Console.WriteLine(car1);
+            Console.WriteLine(car2);
+            Console.WriteLine(truck1);
+            Console.WriteLine(truck2);
+
+            Console.WriteLine(truck1.Equals(truck2));
+            Console.WriteLine(truck1.Equals(truck1));
+        }
+    }
+
+    class Vehicle {
+        // Instance Variables
+        protected Manufacturer man; // Representing the manufacturer of the car
+        protected int numCylinders;
+        protected Person owner;
+
+        // Constructors
+        public Vehicle() { // No Arg Constructor
+            man = Manufacturer.Chevy;
+            numCylinders = 6;
+            owner = new Person();
+        }
+
+        public Vehicle(Manufacturer aMan, int aNum, Person aPerson) { // Constructor with arguments
+            man = aMan;
+            numCylinders = aNum;
+            owner = aPerson;
+        }
+
+        // Getters and Setters
+        public Manufacturer GetManufacturer() {
+            return man;
+        }
+
+        protected void setManufacturer(Manufacturer aMan) {
+            man = aMan;
+        }
+
+        public int getNumCylinders() {
+            return numCylinders;
+        }
+
+        protected void setNumCylinders(int aNum) {
+            numCylinders = aNum;
+        }
+
+        public Person getOwner() {
+            return owner;
+        }
+
+        protected void setOwner(Person aPerson) {
+            owner = aPerson;
+        }
+
+        // Other Methods
+        public override string ToString() {
+            return "Manufacturer: " + GetManufacturer() + "\nNumber of Cylinders: " + getNumCylinders() + "\nOwner: " + getOwner();
+        }
+
+        public bool Equals(Vehicle aVehicle) {
+            if (aVehicle.GetManufacturer() == GetManufacturer() && aVehicle.getOwner() == getOwner() && aVehicle.getNumCylinders() == getNumCylinders()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    class Truck : Vehicle {
+        // Instance Variables
+        double loadCapacity;
+        int towCapacity;
+
+        // Constructors
+        public Truck() { // No Arg
+            man = Manufacturer.Chevy;
+            numCylinders = 6;
+            owner = new Person();
+            loadCapacity = 500;
+            towCapacity = 750;
+        }
+
+        public Truck(Manufacturer aMan, int aNum, Person aPerson, double aLoadCap, int aTowCap) { // Constructors with arguments
+            man = aMan;
+            numCylinders = aNum;
+            owner = aPerson;
+            loadCapacity = aLoadCap;
+            towCapacity = aTowCap;
+        }
+
+        // Getters and Setters
+        public double getLoadCapacity() {
+            return loadCapacity;
+        }
+
+        private void setLoadCapacity(double aLoadCap) {
+            loadCapacity = aLoadCap;
+        }
+
+        public int getTowCapacity() {
+            return towCapacity;
+        }
+
+        private void setTowCapacity(int aTowCap) {
+            towCapacity = aTowCap;
+        }
+
+        // Other Methods
+        public override String ToString() {
+            return base.ToString() + "\nLoad Capacity: " + getLoadCapacity() + "\nTow Capacity: " + getTowCapacity();
+        }
+    }
+
+    class Person {
+        // Instance Variables
+        private string name;
+
+        // Constructors
+        public Person() { // Default No Arg
+            name = "Hugh Mungus";
+        }
+
+        public Person(string aName) { // Constructor with argument
+            name = aName;
+        }
+
+        public Person (Person aPerson) { // Copy Constructor
+            name = aPerson.getName();
+        }
+
+        // Getters and Setters
+        public string getName() {
+            return name;
+        }
+
+        private void setName(string aName) {
+            name = aName;
+        }
+
+        // Other Methods
+        public override string ToString() {
+            return getName();
+        }
+
+        public bool Equals(Object obj) {
+            if (obj.ToString() == ToString()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+
+
+    }
+
+    // Enum
+    public enum Manufacturer {
+        Chevy,
+        Ford,
+        Toyota,
+        Honda,
+        Fiat
+    }
+}
